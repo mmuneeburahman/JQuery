c2a38d62ceae153ab343a02621a17488c132d965
mmuneeburahman <56772228+mmuneeburahman@users.noreply.github.com>
2021-07-28 09:42:36 +0500

    Event Listening

--- index.js
+++ index.js
@@ -24,4 +24,38 @@ $("img").attr("src", "drum.jpg");
 $("a").attr("href", "https://www.yahoo.com");
 
 console.log($("h1").attr("class"));
+  $("h1").click(function(){
+    $("h1").addClass("big-title margin-50");
+    $("h1").css("color", "purple");
+  setTimeout(function(){
+    $("h1").removeClass("big-title");
+    $("h1").css("color", "black");
+  }, 300)
+})
 
+$("button").click(function(){
+  console.log($("h1").css("color"));
+  if(($("h1").css("color"))==="rgb(0, 0, 0)")
+    $("h1").css("color", "pink");
+  else
+    $("h1").css("color", "black");
+})
+
+$(document).keypress(function(event){
+  $("h1").text(event.key);
+  console.log(event.key);
+})
+
+$("button").on("mouseover",function(){
+  $("button").css("font-size", "120%");
+  // setTimeout(function(){
+  //   $("button").css("font-size", "100%");
+  // }, 200)
+})
+
+$("button").on("mouseleave",function(){
+  $("button").css("font-size", "100%");
+  // setTimeout(function(){
+   
+  // }, 200)
+})
