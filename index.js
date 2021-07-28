1f7f1cf3433c17191a972fc727bda71558ec133f
mmuneeburahman <56772228+mmuneeburahman@users.noreply.github.com>
2021-07-28 08:59:25 +0500

    text manipulation

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ $("h1").addClass("big-title margin-50");
 setTimeout(function(){
   $("h1").removeClass("big-title");
 }, 3000)
+
+$("h1").text("Good Bye");
+$("button").text("Don't Click Me");
+$("button").html("Don't <i>Click</i> Me");
+
+var btns = $("button")
+btns.set(0).text("Click Me");
+console.log(btns.get(0));
+console.log(btns);
+// btns[0].text("Click Me");
+// btns[0].css("background-color", "green");
