d2969b10eee90557069b84dce583624af4a4b6c8
mmuneeburahman <56772228+mmuneeburahman@users.noreply.github.com>
2021-07-28 08:49:04 +0500

    style manipulation

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 // $("document").ready(function(){
 //  
 // })
-$("h1").css("color", "red");
+$("Button").css("background-color", "red");
+$("Button").css("color", "white");
+//Same $ is used for querySelector and querySelectorAll.
+
+console.log($("button").css("color"));
+console.log($("button").css("fontSize"));
+console.log($("button").css("fontFamily"));
+
+$("h1").addClass("big-title margin-50");
+setTimeout(function(){
+  $("h1").removeClass("big-title");
+}, 3000)
